d63792d44262d3dbab16f3c5805063615b88587d
GautamKumar200510 <gautamkr.71019@gmail.com>
2026-05-30 19:42:09 +0530

    CollectionFramework

--- src/Linklist.java
+++ src/Linklist.java
@@ -1,7 +1,57 @@
-// Head = [3 | next] [4 ] 
-public class Linklist{
- 
+// Node Class
+class Node {
 
-    
+    int data;
+    Node next;
 
+    public Node(int data) {
+        this.data = data;
+        this.next = null;
+    }
+}
+
+// Linked List Class
+class Linkedlist {
+
+    Node head;
+
+    void insertAtBeginning(int data) {
+
+        Node newNode = new Node(data);
+
+        newNode.next = head;
+
+        head = newNode;
+    }
+
+    void display() {
+
+        Node current = head;
+
+        while (current != null) {
+
+            System.out.print(current.data + " -> ");
+
+            current = current.next;
+        }
+
+        System.out.println("kuch bhi nhi");
+    }
+}
+
+// Main Class
+public class Linklist {
+
+    public static void main(String[] args) {
+
+        Linkedlist list = new Linkedlist();
+
+        list.insertAtBeginning(10);
+        list.insertAtBeginning(20);
+        list.insertAtBeginning(30);
+
+        list.display();
+
+        
+    }
 }
